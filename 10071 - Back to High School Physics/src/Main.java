71409abd1f4047faecffb87ebebd53ce3398bc33
DANILO <danilovasic03@gmail.com>
2023-01-05 12:34:07 +0100

    uva solution 10071

---
+++ 10071 - Back to High School Physics/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+
+        Scanner s = new Scanner(System.in);
+
+        while(s.hasNext()){
+            int v = s.nextInt();
+
+            int t = s.nextInt();
+
+            System.out.println(2*(v*t));
+        }
+    }
+}
