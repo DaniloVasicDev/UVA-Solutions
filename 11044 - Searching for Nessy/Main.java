be4349a16a7d20bca2e0960f351f87f01bed819a
DANILO <danilovasic03@gmail.com>
2023-01-10 16:51:46 +0100

    update code

--- 11044 - Searching for Nessy/Main.java
+++ 11044 - Searching for Nessy/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
 
+        int x = s.nextInt();
+        int i = 0;
+        while(i<x){
+            int n  = s.nextInt();
+            int m = s.nextInt();
+            System.out.println((n/3)*(m/3));
+            i++;
+        }
+        s.close();
     }
 }
