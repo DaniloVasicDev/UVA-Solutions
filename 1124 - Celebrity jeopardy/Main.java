d05e71fd30f5f26e6aa958f48c5986d3a8a331cc
DANILO <danilovasic03@gmail.com>
2023-01-10 15:55:44 +0100

    fix code

---
+++ 1124 - Celebrity jeopardy/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext())
+        {
+            String s = scanner.nextLine();
+            System.out.println(s);
+        }
+    }
+}
