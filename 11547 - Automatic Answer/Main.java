cd2ea9ac08042719f33cce000702199067fefd85
DANILO <danilovasic03@gmail.com>
2023-01-11 11:11:58 +0100

    11547 - Automatic Answer solve

---
+++ 11547 - Automatic Answer/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int t  = s.nextInt();
+        int i = 0;
+
+        while(i<t){
+            int n = s.nextInt();
+            System.out.println((((((((n*567)/9)+7492)*235)/47)-498)/10)%10);
+            i++;
+        }
+        s.close();
+    }
+}
