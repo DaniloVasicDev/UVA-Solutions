055e1ada429cfb70d95695568f043c1ef6d823f7
DANILO <danilovasic03@gmail.com>
2023-01-12 11:55:15 +0100

    12372-Packing for Holiday solve

--- 12372 - Packing for Holiday/Main.java
+++ 12372 - Packing for Holiday/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
 
+        int t = s.nextInt();
+        int x = 1;
+        for(int i=0;i<t;i++){
+            int l = s.nextInt();
+            int w = s.nextInt();
+            int h = s.nextInt();
+            if(l<=20 && w<=20 && h<=20){
+                System.out.println("Case "+x+": good");
+                x++;
+            }
+            else{
+                System.out.println("Case "+x+": bad");
+                x++;
+            }
+        }
     }
 }
