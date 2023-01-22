9f240bf0d5cc3738282531571b0b09a1080bd3d6
DANILO <danilovasic03@gmail.com>
2023-01-22 22:19:08 +0100

    solve half a problem

---
+++ 11078 - Open Credit System/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        while(s.hasNextInt()){
+            int n = s.nextInt();
+            for(int i=0;i<n;i++){
+
+            }
+        }
+    }
+}
