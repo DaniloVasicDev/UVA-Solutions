f15302ba8345a352f07d341fab60410220946784
DANILO <danilovasic03@gmail.com>
2023-01-23 17:02:52 +0100

    wrote a little bit of code

--- 11078 - Open Credit System/Main.java
+++ 11078 - Open Credit System/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        while(s.hasNextInt()){
+        int t = s.nextInt();
+        for(int i=0;i<t;i++){
             int n = s.nextInt();
-            for(int i=0;i<n;i++){
-
+            for(int j=0;j<n;j++){
+              int student = s.nextInt();
+              System.out.println(student);
             }
         }
+        s.close();
     }
 }
