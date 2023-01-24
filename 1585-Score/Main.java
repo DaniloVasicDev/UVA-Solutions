d648614b1d83cd8ec07cb5d62d0cc48093e65ed7
DANILO <danilovasic03@gmail.com>
2023-01-24 13:41:22 +0100

    solve problem

--- 1585-Score/Main.java
+++ 1585-Score/Main.java
@@ -4,28 +4,25 @@ public class Main {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
+        s.nextLine();
         int numOf0 = 0;
         int sum =0;
         int x =0;
         while(x<t){
             String word = s.nextLine().toUpperCase();
-            boolean isPrevoisX = true;
             for(int i=0;i<word.length();i++) {
-                if (word.charAt(i) == 'O' && isPrevoisX) {
-                    numOf0++;
-                    isPrevoisX = false;
-                }
-                else if(word.charAt(i) == 'O' && !isPrevoisX){
+                if (word.charAt(i) == 'O') {
                     numOf0++;
                 }
                 else if (word.charAt(i) == 'X') {
-                    isPrevoisX = true;
                     numOf0 = 0;
                 }
                  sum+=numOf0;
             }
             System.out.println(sum);
             x++;
+            sum = 0;
+            numOf0 =0;
         }
     }
 }
