5541065ee99a0ca18e0fb869698b2ca76e436d4e
DANILO <danilovasic03@gmail.com>
2023-01-25 10:41:14 +0100

    solve google is felling lucky problem

--- 12015-Google is Felling Lucky/Main.java
+++ 12015-Google is Felling Lucky/Main.java
@@ -1,4 +1,5 @@
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -6,17 +7,32 @@ public class Main {
         Scanner s = new Scanner(System.in);
         PrintStream out = new PrintStream(System.out);
         int t = s.nextInt();
+        s.nextLine();
         int x =0;
+        int a  =1;
+        int max = 0;
+        ArrayList<String> arrayList = new ArrayList<>();
         while (x<t){
             for(int i=0;i<10;i++){
-                String web = s.nextLine();
-                int rel = s.nextInt();
-                int max = rel;
-                if(){
-
+                arrayList.add(s.nextLine());
+            }
+            for(int i=0;i< arrayList.size();i++){
+                String[] num = arrayList.get(i).split(" ");
+                if(Integer.parseInt(num[1])>max){
+                    max = Integer.parseInt(num[1]);
                 }
             }
+            out.println("Case #"+a+":");
+            for(int i=0;i<arrayList.size();i++){
+                String[] num = arrayList.get(i).split(" ");
+                if(max == Integer.parseInt(num[1])){
+                    out.println(num[0]);
+                }
+            }
+            arrayList.clear();
             x++;
+            a++;
+            max = 0;
         }
     }
 }
